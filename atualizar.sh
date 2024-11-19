0a1b0857195a60662677134f0ce41414e0ca489f
felipe caue machado <felipe.caue.trabalho@gmail.com>
2024-11-19 10:13:25 -0300

    Oitavo commit

--- atualizar.sh
+++ atualizar.sh
@@ -4,6 +4,9 @@ git fetch origin
 
 if [[ -n $(git log HEAD..origin/main --oneline) ]]; then
     echo "Existem novos commits no branch remoto!"
+    echo "Baixando dados do repositorio"
+
+    git pull
 else
     echo "O branch local está atualizado com o remoto."
 fi
